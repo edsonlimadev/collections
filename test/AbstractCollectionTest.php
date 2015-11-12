6cb3c958499c0753250837956c9e614c7590d66f
Edson Lima <dddwebdeveloper@gmail.com>
2015-11-12 02:40:23 -0200

    Fix namespace of abstract collection test

--- test/AbstractCollectionTest.php
+++ test/AbstractCollectionTest.php
@@ -1,15 +1,13 @@
 <?php
 
-namespace Edsonlimadev\Collections\Test;
-
-use Edsonlimadev\Collections;
+namespace Edsonlimadev\Collections;
 
 class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 {
     public function testEach()
     {
         $sum = 0;
-        $collection = new Collections\Immutable([1,2,3]);
+        $collection = new Immutable([1,2,3]);
 
         $collection->each(function($elem) use(&$sum) {
             $sum += (int) $elem;
@@ -21,7 +19,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
     public function testEachWithIndex()
     {
         $mapped = [];
-        $collection = new Collections\Immutable(['a','b','c']);
+        $collection = new Immutable(['a','b','c']);
 
         $collection->eachWithIndex(function($index, $element) use (&$mapped) {
            $mapped[++$index] = $element;
@@ -36,7 +34,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testFilter()
     {
-        $collection = new Collections\Immutable([1,'a',3]);
+        $collection = new Immutable([1,'a',3]);
 
         $filtered = $collection->filter(function($element) {
            return is_int($element);
@@ -48,7 +46,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testMap()
     {
-        $collection = new Collections\Immutable([
+        $collection = new Immutable([
             ['name' => 'Edson', 'age' => 25],
             ['name' => 'Batman', 'age' => 25]
         ]);
@@ -62,7 +60,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testReduceWithoutInitialValue()
     {
-        $collection = new Collections\Immutable(['Edson','Lima', 'Junior']);
+        $collection = new Immutable(['Edson','Lima', 'Junior']);
 
         $this->assertEquals('Edson Lima Junior', $collection->reduce(function($partA, $partB) {
             return trim(sprintf('%s %s', $partA, $partB));
@@ -71,7 +69,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testReduceWithAInitialValue()
     {
-        $collection = new Collections\Immutable([2,2,5]);
+        $collection = new Immutable([2,2,5]);
 
         $this->assertEquals(20, $collection->reduce(function($multiplier, $multiplicand) {
             return $multiplier * $multiplicand;
@@ -80,7 +78,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testEqualsWithASimilarCollection()
     {
-        $collectionA = new Collections\Immutable([42,'vamos mostrar cultura']);
+        $collectionA = new Immutable([42,'vamos mostrar cultura']);
         $collectionB = clone $collectionA;
 
         $this->assertTrue($collectionA->equals($collectionB));
@@ -88,37 +86,37 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testEqualsWithADifferentCollection()
     {
-        $collectionA = new Collections\Immutable([42,'top nos falsetes']);
-        $collectionB = new Collections\Immutable([1 => 42, 'a' =>'top nos falsetes']);
+        $collectionA = new Immutable([42,'top nos falsetes']);
+        $collectionB = new Immutable([1 => 42, 'a' =>'top nos falsetes']);
 
         $this->assertFalse($collectionA->equals($collectionB));
     }
 
     public function testEqualsWithACollectionWithSameValuesButDifferentIndexes()
     {
-        $collectionA = new Collections\Immutable([42,'top nos falsetes, viu!!!']);
-        $collectionB = new Collections\Immutable([1 => 42, 'a' =>'top nos falsetes, viu!!!']);
+        $collectionA = new Immutable([42,'top nos falsetes, viu!!!']);
+        $collectionB = new Immutable([1 => 42, 'a' =>'top nos falsetes, viu!!!']);
 
         $this->assertFalse($collectionA->equals($collectionB));
     }
 
     public function testContains()
     {
-        $collection = new Collections\Immutable(range(40,42));
+        $collection = new Immutable(range(40,42));
 
         $this->assertTrue($collection->contains(42));
     }
 
     public function testToArray()
     {
-        $collection = new Collections\Immutable([42, new \stdClass()]);
+        $collection = new Immutable([42, new \stdClass()]);
 
         $this->assertEquals([42, new \stdClass()], $collection->toArray());
     }
 
     public function testGetIterator()
     {
-        $collection = new Collections\Immutable([1,3]);
+        $collection = new Immutable([1,3]);
         $iterator = $collection->getIterator();
         $values = [];
 
@@ -132,7 +130,7 @@ class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 
     public function testCount()
     {
-        $collection = new Collections\Immutable([1,3]);
+        $collection = new Immutable([1,3]);
 
         $this->assertCount(2, $collection);
     }
